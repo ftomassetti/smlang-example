8fb64cc204af449bac549667f8113f69b0287942
Federico Tomassetti <federico@tomassetti.me>
2016-12-28 11:34:10 +0100

    fix SMLexerTest

--- src/test/kotlin/SMLexerTest.kt
+++ src/test/kotlin/SMLexerTest.kt
@@ -1,7 +1,6 @@
 package me.tomassetti.smlang
 
 import lexerForCode
-import tokensContent
 import tokensNames
 import kotlin.test.assertEquals
 import org.junit.Test as test
@@ -39,40 +38,8 @@ class SMLexerTest {
     }
 
     @org.junit.Test fun parseSimpleString() {
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT", "STRING_CLOSE", "EOF"),
+        assertEquals(listOf("STRINGLIT", "EOF"),
                 tokensNames(lexerForCode("\"hi!\"")))
     }
 
-    @org.junit.Test fun parseStringWithNewlineEscape() {
-        val code = "\"hi!\\n\""
-        assertEquals(listOf("\"", "hi!", "\\n", "\"","EOF"),
-                tokensContent(lexerForCode(code)))
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT", "ESCAPE_NEWLINE", "STRING_CLOSE","EOF"),
-                tokensNames(lexerForCode(code)))
-    }
-
-    @org.junit.Test fun parseStringWithSlashEscape() {
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT", "ESCAPE_SLASH", "STRING_CLOSE","EOF"),
-                tokensNames(lexerForCode("\"hi!\\\\\"")))
-    }
-
-    @org.junit.Test fun parseStringWithDelimiterEscape() {
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT",  "ESCAPE_STRING_DELIMITER", "STRING_CLOSE","EOF"),
-                tokensNames(lexerForCode("\"hi!\\\"\"")))
-    }
-
-    @org.junit.Test fun parseStringWithSharpEscape() {
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT",  "ESCAPE_SHARP", "STRING_CLOSE","EOF"),
-                tokensNames(lexerForCode("\"hi!\\#\"")))
-    }
-
-    @org.junit.Test fun parseStringWithInterpolation() {
-        val code = "\"hi #{name}. This is a number: #{5 * 4}\""
-        assertEquals(listOf("\"", "hi ", "#{", "name", "}", ". This is a number: ", "#{", "5", "*", "4", "}", "\"", "EOF"),
-                tokensContent(lexerForCode(code)))
-        assertEquals(listOf("STRING_OPEN", "STRING_CONTENT", "INTERPOLATION_OPEN", "ID", "INTERPOLATION_CLOSE",
-                "STRING_CONTENT", "INTERPOLATION_OPEN", "INTLIT", "ASTERISK", "INTLIT", "INTERPOLATION_CLOSE",
-                "STRING_CLOSE", "EOF"),
-                tokensNames(lexerForCode(code)))
-    }
 }
