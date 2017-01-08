2f0cb1797df0b72624d6f0eeaa080909626ef373
Federico Tomassetti <federico@tomassetti.me>
2017-01-08 12:32:14 +0100

    example for showing lexer

--- src/main/antlr/SMLexer.g4
+++ src/main/antlr/SMLexer.g4
@@ -54,4 +54,3 @@ ARROW              : '->' ;
 STRINGLIT          : '"' ~["]* '"' ;
 
 UNMATCHED          : . ;
-
--- src/main/kotlin/me/tomassetti/presentation/ShowLexer.kt
+++ src/main/kotlin/me/tomassetti/presentation/ShowLexer.kt
@@ -1,5 +1,36 @@
 package me.tomassetti.presentation
 
-fun main(args: Array<String>) {
+import me.tomassetti.parsing.toParseTree
+import me.tomassetti.smlang.SMLexer
+import me.tomassetti.smlang.SMParser
+import me.tomassetti.smlang.parsing.lexerForCode
+import me.tomassetti.smlang.parsing.parseCode
+import org.antlr.v4.runtime.Token
 
+fun main(args: Array<String>) {
+    val lexer = lexerForCode("""
+            statemachine mySm
+
+            input lowSpeedThroughtput: Int
+            input highSpeedThroughtput: Int
+
+            var totalProduction = 0
+
+            event turnOff
+            event turnOn
+            event speedUp
+            event speedDown
+            event emergencyStop
+            event doNothing
+
+
+            start state turnedOff {
+                on turnOn -> turnedOn
+            }""")
+    var token : Token? = null
+    do {
+        token = lexer.nextToken()
+        val typeName = SMLexer.VOCABULARY.getSymbolicName(token.type)
+        println("L${token.line}(${token.startIndex}-${token.stopIndex}) $typeName '${token.text}'")
+    } while (token?.type != -1)
 }
